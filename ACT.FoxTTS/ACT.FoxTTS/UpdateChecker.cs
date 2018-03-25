98b741dbce1b554ba185b0c5b44538e6b77e4a9a
noisyfox <timemanager.rick@gmail.com>
2018-03-26 02:11:04 +1100

    Fix update

--- ACT.FoxTTS/ACT.FoxTTS/UpdateChecker.cs
+++ ACT.FoxTTS/ACT.FoxTTS/UpdateChecker.cs
@@ -5,12 +5,12 @@ namespace ACT.FoxTTS
 {
     public class UpdateChecker : UpdateCheckerBase<MainController, FoxTTSPlugin>
     {
-        public const string ReleasePage = "https://github.com/Noisyfox/ACT.FoxTTSPlugin/releases";
+        public const string ReleasePage = "https://github.com/Noisyfox/ACT.FoxTTS/releases";
 
-        protected override string UpdateUrl { get; } = "https://api.github.com/repos/Noisyfox/ACT.FoxTTSPlugin/releases";
+        protected override string UpdateUrl { get; } = "https://api.github.com/repos/Noisyfox/ACT.FoxTTS/releases";
 
         private const string NameMatcher =
-            @"^ACT\.FoxTTSPlugin(?:-|\.)(?<version>\d+(?:\.\d+)*)(?:|-Release)\.7z$";
+            @"^ACT\.FoxTTS(?:-|\.)(?<version>\d+(?:\.\d+)*)(?:|-Release)\.7z$";
 
         protected override string ParseVersion(string fileName)
         {
